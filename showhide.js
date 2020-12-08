5f55e1593e93b069933e3c838619fe9c536d56d0
CPR3003 <crmaynooth@gmail.com>
2020-12-08 12:16:56 +0000

    third-commit

---
+++ showhide.js
@@ -0,0 +1,15 @@
+function showhide() {
+    
+    var x = document.getElementById("myform");
+    
+    if(x.style.display==="none") {
+    
+        x.style.display="block"
+    }
+   
+    else {
+        x.style.display="none";
+    }
+    
+    
+}
